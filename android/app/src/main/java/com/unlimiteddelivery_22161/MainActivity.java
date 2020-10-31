81bcffde9a9703b3689a7cac6bf4d998335832be
Crowdbotics <team@crowdbotics.com>
2020-10-31 17:11:25 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/unlimiteddelivery_22161/MainActivity.java
@@ -0,0 +1,15 @@
+package com.unlimiteddelivery_22161;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "unlimiteddelivery_22161";
+    }
+}
